e49e038443f6ee5c6d7e87c664a93a0ca051c058
Daniel Gohl <daniel@prism11.paricarana.ufrr.br>
2019-05-27 18:00:17 -0400

    Added frame_cap to webapp

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_from_directory, request
-
+from frame_cap import frame_cap
 
 app = Flask(__name__)
 
@@ -10,3 +10,10 @@ def index():
 @app.route('/assets/<path:path>')
 def assets(path):
     return send_from_directory("web/assets", path)
+
+@app.route("/cap")
+def cap():
+    ip = request.args.get("ip", "")
+    timeout = request.args.get("timeout", 5.0)
+    fps = request.args.get("fps", 24)
+    return frame_cap(ip, fps, timeout)
