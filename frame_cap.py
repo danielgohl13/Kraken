7114d81e1228627c24c4c801fde88de5f2925187
Daniel Gohl <daniel@prism11.paricarana.ufrr.br>
2019-05-27 17:59:47 -0400

    Added frame_cap function to module

--- frame_cap.py
+++ frame_cap.py
@@ -1,45 +1,52 @@
 #!/usr/bin/env python3
 import time
 import urllib.request
+import sys
 
 import cv2
 import numpy as np
 
 import object_count
 
-PATH_IN = "http://192.168.0.100:8080/shot.jpg"
-PATH_OUT = "cam.mp4"
-FPS = 24.0
-TIMEOUT = 5.0
 
-FRAME_ARRAY = []
-START = time.time()
-while True:
-    print(".", end="")
-    #reading each FRAME
-    RESP = urllib.request.urlopen(PATH_IN)
-    FRAME = np.asarray(bytearray(RESP.read()), dtype="uint8")
-    FRAME = cv2.imdecode(FRAME, cv2.IMREAD_COLOR)
+def frame_cap(ip, fps: int = 24, timeout: float = 5.0):
+    path_in = "http://" + ip + "/shot.jpg"
+    path_out = "cam.mp4"
 
-    height, width, layers = FRAME.shape
-    size = (width,height)
+    frame_array = []
+    start = time.time()
+    size = (0, 0)
+    while (time.time() - start < timeout):
+        print(".", end="")
+        # reading each FRAME
+        RESP = urllib.request.urlopen(path_in)
+        FRAME = np.asarray(bytearray(RESP.read()), dtype="uint8")
+        FRAME = cv2.imdecode(FRAME, cv2.IMREAD_COLOR)
 
-    cv2.imshow("imagem", FRAME)
+        height, width, layers = FRAME.shape
+        size = (width, height)
 
-    #inserting the frames into an image array
-    FRAME_ARRAY.append(FRAME)
+        cv2.imshow("imagem", FRAME)
 
-    if (cv2.waitKey(1) & 0xFF == ord('q')) or (time.time() - START >= TIMEOUT):
-        break
+        # inserting the frames into an image array
+        frame_array.append(FRAME)
 
-    time.sleep(1/FPS)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-out = cv2.VideoWriter(PATH_OUT, cv2.VideoWriter_fourcc(*'mp4v'), FPS, size)
+        time.sleep(1/fps)
 
-for img in FRAME_ARRAY:
-    # writing to a image array
-    out.write(img)
+    out = cv2.VideoWriter(path_out, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
 
-out.release()
-cv2.destroyAllWindows()
-object_count.process(PATH_OUT)
+    for img in frame_array:
+        # writing to a image array
+        out.write(img)
+
+    out.release()
+    cv2.destroyAllWindows()
+    print("object counting...")
+    object_count.process(path_out)
+
+
+if __name__ == "__main__":
+    frame_cap(ip=sys.argv[1], fps=int(sys.argv[2]), timeout=int(sys.argv[3]))
